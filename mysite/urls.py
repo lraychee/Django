7c852456914a04927efcb6d51f29c5e54278f54e
Rachel <45116399+lraychee@users.noreply.github.com>
2025-12-22 13:39:53 +0800

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # 把 core 的 URL 接進來
 ]
+from django.contrib import admin
